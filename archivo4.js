4e74c394612432ee899ce6fe286ed473f1508cac
Arturo <118952973+bossy3k@users.noreply.github.com>
2023-02-25 10:06:15 -0600

    Add files via upload

---
+++ archivo4.js
@@ -0,0 +1,14 @@
+var indice = []; 
+indice[0] ="arturo";
+indice[3] = "maria";
+
+let indexOfThree = indice.indexOf("arturo");
+
+var datos=[];
+datos[0]='2288525068';
+datos[1]='2293434343';
+datos[2]='2294454334';
+datos[3]='7322333244';
+datos[5]='2345667788';
+
+console.log('Valor buscado:'+datos[indexOfThree]);
